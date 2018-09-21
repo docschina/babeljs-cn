78f9e7f8a164f6f26485244d68a902b65a6f74f1
QC-L <github@liqichang.com>
2018-09-21 13:43:07 +0800

    fix: baidu hmt js

--- website/static/scripts/hmt.js
+++ website/static/scripts/hmt.js
@@ -1,6 +1,7 @@
+var _hmt = _hmt || [];
 (function() {
-  const hm = document.createElement("script");
+  var hm = document.createElement("script");
   hm.src = "https://hm.baidu.com/hm.js?7d7e96af885ecd66a8c4ac73dc983681";
-  const s = document.getElementsByTagName("script")[0];
+  var s = document.getElementsByTagName("script")[0];
   s.parentNode.insertBefore(hm, s);
 })();
