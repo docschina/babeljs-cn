63faaa4e77ce829d12ef4df4241f4fb15323aee7
dear-lizhihua <275091674@qq.com>
2021-03-17 23:35:57 +0800

    fix: 合并错误

--- website/pages/en/setup.js
+++ website/pages/en/setup.js
@@ -98,15 +98,9 @@ const StepFour = () => {
     <div className="step-hidden step-setup">
       <h2>
         <span className="step-no">4</span>
-<<<<<<< HEAD
-        {"创建"}
-        <code>.babelrc</code>
-        {" 配置文件"}
-=======
-        <translate desc="setup page - step 4 one">Create</translate>{" "}
+        <translate desc="setup page - step 4 one">创建</translate>{" "}
         <code>babel.config.json</code>{" "}
-        <translate desc="setup page - step 4 two">configuration file</translate>
->>>>>>> 3ab4f32200f518a45063f2560684b8873bd8603b
+        <translate desc="setup page - step 4 two">配置文件</translate>
       </h2>
       <MarkdownBlock>{setupBabelrc}</MarkdownBlock>
     </div>
