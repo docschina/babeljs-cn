5261a874d36560edd536f8dbc77b4bd6e8feeebe
QC-L <github@liqichang.com>
2021-05-04 22:37:56 +0800

    fix: build error

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -69,172 +69,78 @@ const GetStarted = ({ language }) => {
       }}
     >
       <p>
-        了解更多关于 Babel 的信息{" "}
+        了解更多关于 Babel 的信息{""}
         <a href={siteConfig.getDocUrl("index.html", language)}>
           入门指南
-        </a>{" "}
-        或者翻阅{" "}
+        </a>{""}
+        或者翻阅{""}
         <a href={siteConfig.getPageUrl("videos.html", language)}>视频</a>
         了解更多关于 Babel 的故事
       </p>
       <p>
         我们只是一个小的
-        <a href={siteConfig.getPageUrl("team.html", language)}>志愿者</a>{" "}
+        <a href={siteConfig.getPageUrl("team.html", language)}>志愿者团体</a>{""}
         在社区的赞助下，业余时间维护这个项目。
         如果 Babel 在你工作中使你受益颇丰，可以成为
         <a href="https://github.com/babel/babel/blob/master/CONTRIBUTING.md">
           贡献者
-        </a>{" "}
+        </a>{""}
         或者也可以<a href="https://opencollective.com/babel">赞助</a>我们！
       </p>
     </div>
   );
 };
 
-const SponsorTier = props => {
-  let { min, max } = props;
-  const tierSponsors = siteConfig.sponsors.filter(sponsor => {
-    let value = Math.max(sponsor.monthly, sponsor.yearly / 12);
-    return +value >= min && (!max || (max && +value < max));
-  });
+const HomeContainer = props => (
+  <div
+    className="container"
+    style={{ backgroundColor: "#f6f6f6", paddingBottom: 20 }}
+  >
+    <div className="wrapper">
+      <div className="gridBlock">{props.children}</div>
+    </div>
+  </div>
+);
+
+const Hero = ({ language }) => (
+  <div className="hero">
+    <a href="https://teespring.com/babel-christmas?pr=FLAVORTOWN">
+      <div className="homepage-banner">获取 Babel 的假日服饰👕</div>
+    </a>
+    <div className="hero__container">
+      <h1>
+        <translate>Babel 是一个 JavaScript compiler。</translate>
+      </h1>
+      <p>
+        <translate>现在就开始使用下一代 JavaScript 语法吧。</translate>
+      </p>
+
+      <div className="hero__announcement">
+        <span>
+          <strong>Babel 7.13 发布！</strong> 请阅读我们的{" "}
+          <a href="blog/2021/02/22/7.13.0">博客公告</a> 以及{" "}
+          <a href="https://github.com/babel/babel/releases/tag/v7.13.0">changelog</a> 了解更多详情！
+      </span>
+      </div>
+
+      <DummyMiniRepl />
+      <MiniRepl language={language} />
+    </div>
+  </div>
+);
+
+const Index = ({ language }) => {
   return (
     <div>
-      <h3>{props.title}</h3>
-      <div>
-        are currently pledging or have donated an average of{" "}
-        {max ? `$${min}-$${max}` : `>$${min}`}
-        /month in the last year{" "}
+      <Hero language={language} />
+
+      <div className="mainContainer" style={{ padding: 0 }}>
+        <HomeContainer>
+          <GetStarted language={language} />
+        </HomeContainer>
       </div>
-      <br />
-      <ul className={`sponsors-tier tier-${props.tier}`}>
-        {tierSponsors.map((sponsor, i) => (
-          <li key={i}>
-            <a
-              href={sponsor.url}
-              title={sponsor.name}
-              target="_blank"
-              rel="noopener sponsored"
-            >
-              <img src={sponsor.image} alt={`Sponsored by ${sponsor.name}`} />
-            </a>
-          </li>
-        ))}
-      </ul>
-      {props.button ? (
-        <PromoSection>
-          <Button href={props.button.link} target="_blank">
-            {props.button.title}
-          </Button>
-        </PromoSection>
-      ) : null}
     </div>
   );
 };
 
-const OpenCollectiveSponsors = () => {
-  const ocButton = {
-    title: "成为赞助商",
-    link: "https://opencollective.com/babel",
-  },
-    patreonButton = {
-      title: "成为赞助人",
-      link: "https://www.patreon.com/join/henryzhu",
-    };
-
-  const OpenCollectiveSponsors = ({ language }) => {
-    return (
-      <div className="container paddingBottom">
-        <div className="wrapper productShowcaseSection">
-          <h3>Current Sponsors</h3>
-          <p>
-            We&apos;re a small group of{" "}
-            <a href={siteConfig.getPageUrl("team.html", language)}>volunteers</a>{" "}
-          that spend their free time maintaining this project, funded by the
-          community. If Babel has benefited you in your work, becoming a{" "}
-            <a href="https://github.com/babel/babel/blob/master/CONTRIBUTING.md">
-              contributor
-          </a>{" "}
-          or <a href="https://opencollective.com/babel">sponsoring</a> might
-          just be a great way to give back!
-        </p>
-          <div className="sponsor-tiers" id="sponsors">
-            <SponsorTier
-              type="opencollective"
-              title="Base Support"
-              tier="base-support-sponsors"
-              min={2000}
-            />
-            <SponsorTier
-              type="opencollective"
-              title="金牌赞助（Open Collective）"
-              tier="gold-sponsors"
-              min={1000}
-              max={2000}
-            />
-            <SponsorTier
-              type="opencollective"
-              title="Silver"
-              tier="silver-sponsors"
-              min={500}
-              max={1000}
-            />
-          </div>
-        </div>
-      </div>
-    );
-  };
-
-  const HomeContainer = props => (
-    <div
-      className="container"
-      style={{ backgroundColor: "#f6f6f6", paddingBottom: 20 }}
-    >
-      <div className="wrapper">
-        <div className="gridBlock">{props.children}</div>
-      </div>
-    </div>
-  );
-
-  const Hero = ({ language }) => (
-    <div className="hero">
-      <a href="https://teespring.com/babel-christmas?pr=FLAVORTOWN">
-        <div className="homepage-banner">获取 Babel 的假日服饰👕</div>
-      </a>
-      <div className="hero__container">
-        <h1>
-          <translate>Babel 是一个 JavaScript compiler。</translate>
-        </h1>
-        <p>
-          <translate>现在就开始使用下一代 JavaScript 语法吧。</translate>
-        </p>
-
-        <div className="hero__announcement">
-          <span>
-            <strong>Babel 7.13 发布！</strong> 请阅读我们的{" "}
-            <a href="blog/2021/02/22/7.13.0">博客公告</a> 以及{" "}
-            <a href="https://github.com/babel/babel/releases/tag/v7.13.0">changelog</a> 了解更多详情！
-        </span>
-        </div>
-
-        <DummyMiniRepl />
-        <MiniRepl language={language} />
-      </div>
-    </div>
-  );
-
-  const Index = ({ language }) => {
-    return (
-      <div>
-        <Hero language={language} />
-
-        <div className="mainContainer" style={{ padding: 0 }}>
-          <HomeContainer>
-            <GetStarted language={language} />
-          </HomeContainer>
-          <OpenCollectiveSponsors language={language} />
-        </div>
-      </div>
-    );
-  };
-
-  module.exports = Index;
+module.exports = Index;
